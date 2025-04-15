10ab2f56598b864d212d87463a96d07c3b8ae804
Nithish Bilasunur Manjunatha Reddy <43351617+nitheshbethur39@users.noreply.github.com>
2025-04-15 19:21:57 -0400

    updated 3 quarter to day trading

--- src/components/ChartSelector.tsx
+++ src/components/ChartSelector.tsx
@@ -1,6 +1,5 @@
-
 import React from 'react';
-import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 interface ChartSelectorProps {
   onPeriodChange: (period: string) => void;
@@ -11,7 +10,7 @@ const ChartSelector: React.FC<ChartSelectorProps> = ({ onPeriodChange }) => {
     <Tabs defaultValue="2q" className="w-full" onValueChange={onPeriodChange}>
       <TabsList className="mb-4">
         <TabsTrigger value="2q">2 Quarters</TabsTrigger>
-        <TabsTrigger value="3q">3 Quarters</TabsTrigger>
+        <TabsTrigger value="day">Day Trading</TabsTrigger>
       </TabsList>
     </Tabs>
   );
